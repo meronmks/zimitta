d1c7f521a63cc804647f15ce102c2cd099ec24dc
meronmks <meronmks.8914@gmail.com>
2016-09-13 15:58:12 +0900

    設定の読み出しの整理

--- app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
+++ app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
@@ -1,5 +1,7 @@
 package com.meronmks.zimitta.Core;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,11 +14,19 @@ import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(OAuthVariable.TWITTER_KEY, OAuthVariable.TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(getString(R.string.Account), 0);
+
+        if(!OauthUtils.hasAccessToken(this, preferences.getLong(getString(R.string.ActiveAccount), 0))){
+
+        }
     }
 }
