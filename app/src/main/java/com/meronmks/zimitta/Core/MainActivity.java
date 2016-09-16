bb2d884d6f2ee89d594d7b3e2eb6d138d2ca52a4
meronmks <meronmks.8914@gmail.com>
2016-09-16 21:33:44 +0900

    ユーザーIDとミュートIDリスト取得メソッド追加

--- app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
+++ app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
@@ -8,21 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.meronmks.zimitta.Adapter.MainPagerAdapter;
 import com.meronmks.zimitta.Datas.Variable;
 import com.meronmks.zimitta.OAuth.OAuthVariable;
 import com.meronmks.zimitta.OAuth.OauthUtils;
 import com.meronmks.zimitta.OAuth.TwitterOAuthActivity;
 import com.meronmks.zimitta.R;
+import com.meronmks.zimitta.TwitterUtil.TwitterAction;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
 import io.fabric.sdk.android.Fabric;
+import twitter4j.IDs;
+import twitter4j.TwitterAdapter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterListener;
+import twitter4j.TwitterMethod;
+import twitter4j.User;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences preferences;
     private static Context mContext;
+    private TwitterAction mAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +56,20 @@ public class MainActivity extends AppCompatActivity {
             MainPagerAdapter pagerAdapter = new MainPagerAdapter(fragmentManager,this);
             ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
             viewPager.setAdapter(pagerAdapter);
+            mAction = new TwitterAction(this, listener);
+            mAction.getVerifyCredentials();
+            mAction.getMutesIDs();
         }
+
+        RxView.clicks(findViewById(R.id.tweet))
+                .subscribe(x -> {
+
+                });
+
+        RxView.clicks(findViewById(R.id.Menu_button))
+                .subscribe(x -> {
+
+                });
     }
 
     @Override
@@ -62,7 +84,45 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
+    /**
+     * Listener定義
+     */
+    private TwitterListener listener = new TwitterAdapter() {
+        @Override
+        public void verifiedCredentials(User user) {
+            if(user != null){
+                Variable.userID = user.getId();
+                Variable.userName = user.getScreenName();
+            }
+        }
 
+        @Override
+        public void gotMuteIDs(IDs blockingUsersIDs) {
+            if(blockingUsersIDs == null){
+                Variable.muteList = null;
+                Variable.muteList = new long[1];
+                Variable.muteList[0] = 0;
+            }else{
+                Variable.muteList = null;
+                Variable.muteList = new long[blockingUsersIDs.getIDs().length];
+                Variable.muteList = blockingUsersIDs.getIDs();
+            }
+        }
+
+        @Override
+        public void onException(TwitterException te, TwitterMethod method) {
+            runOnUiThread(() -> {
+                switch (method){
+                    case VERIFY_CREDENTIALS:
+                        showToast("ユーザの情報取得に失敗しました");
+                        break;
+                    case MUTE_LIST_IDS:
+                        showToast("ミュート一覧の取得に失敗しました");
+                        break;
+                }
+            });
+        }
+    };
 
     /**
      * トースト表示処理
