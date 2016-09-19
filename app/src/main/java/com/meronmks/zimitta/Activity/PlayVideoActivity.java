3f33a8db1f5979803ee9b73b3843959d858b9c72
meronmks <meronmks.8914@gmail.com>
2016-09-20 08:54:04 +0900

    読み込み中表示を追加

--- app/src/main/java/com/meronmks/zimitta/Activity/PlayVideoActivity.java
+++ app/src/main/java/com/meronmks/zimitta/Activity/PlayVideoActivity.java
@@ -1,11 +1,15 @@
 package com.meronmks.zimitta.Activity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 import com.meronmks.zimitta.R;
@@ -15,7 +19,9 @@ import com.meronmks.zimitta.R;
  */
 public class PlayVideoActivity extends AppCompatActivity {
 
-    VideoView videoView;
+    VideoView mVideoView;
+    ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +31,18 @@ public class PlayVideoActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String videoURL = intent.getStringExtra("Video");
 
-        videoView = (VideoView) findViewById(R.id.VideoView);
-        videoView.setMediaController(new MediaController(this));
-        videoView.setVideoURI(Uri.parse(videoURL));
+        mProgressBar = (ProgressBar) findViewById(R.id.VideoLoadingProgress);
+
+        mVideoView = (VideoView) findViewById(R.id.VideoView);
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.setVideoURI(Uri.parse(videoURL));
+
+        mVideoView.setOnPreparedListener(mediaPlayer -> {
+            mediaPlayer.start();
+            mediaPlayer.setOnVideoSizeChangedListener((mediaPlayer1, i, i1) -> {
+                mProgressBar.setVisibility(View.GONE);
+                mediaPlayer.start();
+            });
+        });
     }
 }
