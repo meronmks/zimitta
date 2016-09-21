4a1b381f5cd5fc6e2094b6c132347e0f8c5f959a
meronmks <meronmks.8914@gmail.com>
2016-09-21 21:21:40 +0900

    レートリミットの情報をまとめておくクラスの作成

---
+++ app/src/main/java/com/meronmks/zimitta/Datas/RateLimits.java
@@ -0,0 +1,11 @@
+package com.meronmks.zimitta.Datas;
+
+import twitter4j.RateLimitStatus;
+
+/**
+ * Created by meron on 2016/09/21.
+ */
+public class RateLimits {
+    RateLimitStatus Home;
+    RateLimitStatus Mention;
+}
