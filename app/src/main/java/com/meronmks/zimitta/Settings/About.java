7a58297c31dae2c8e5d59133ad0ca0e7a3446f8f
meronmks <meronmks.8914@gmail.com>
2016-09-23 22:07:34 +0900

    About画面への移動を作成

---
+++ app/src/main/java/com/meronmks/zimitta/Settings/About.java
@@ -0,0 +1,17 @@
+package com.meronmks.zimitta.Settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.meronmks.zimitta.R;
+
+/**
+ * Created by meron on 2016/09/23.
+ */
+public class About extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/meronmks/zimitta/Settings/SettingFragment.java
+++ app/src/main/java/com/meronmks/zimitta/Settings/SettingFragment.java
@@ -1,21 +1,39 @@
 package com.meronmks.zimitta.Settings;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.meronmks.zimitta.R;
 
 /**
  * Created by meron on 2016/09/21.
  */
-public class SettingFragment extends PreferenceFragment {
+public class SettingFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
+        ListPreference list_preference = (ListPreference)getPreferenceScreen().findPreference("LoadTweetCount");
+        list_preference.setSummary(list_preference.getEntry());
+
+        Preference nextMove1 = findPreference("About");
+        nextMove1.setOnPreferenceClickListener(preference -> {
+            Intent nextActivity = new Intent(getActivity() ,About.class);
+            startActivity(nextActivity);
+            return true;
+        });
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
         ListPreference list_preference = (ListPreference)getPreferenceScreen().findPreference("LoadTweetCount");
         list_preference.setSummary(list_preference.getEntry());
     }
