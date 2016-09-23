47ce150330d3f6d7a584889e8643ee380b0fca73
meronmks <meronmks.8914@gmail.com>
2016-09-23 22:19:04 +0900

    dev2の準備

--- app/src/main/java/com/meronmks/zimitta/Settings/About.java
+++ app/src/main/java/com/meronmks/zimitta/Settings/About.java
@@ -12,6 +12,6 @@ public class About extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
     }
 }
