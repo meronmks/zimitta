07368f153da0690ac9d71b082fb95424f6d41d6f
meronmks <meronmks.8914@gmail.com>
2016-10-02 01:05:32 +0900

    削除

--- app/src/main/java/com/meronmks/zimitta/OAuth/OAuthVariable.java
+++
@@ -1,9 +0,0 @@
-package com.meronmks.zimitta.OAuth;
-
-/**
- * Created by meron on 2016/09/14.
- */
-public class OAuthVariable {
-    public static final String TWITTER_KEY = "Av2STDatcnhhku4bEuVWA";
-    public static final String TWITTER_SECRET = "YfJUh3GtdnSMmiaYv0LUSXd2gSpkbTFDF08EcQ14";
-}
