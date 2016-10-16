ee9faa60d8f898dc4f2aec395af6ee8a1b8423e2
meronmks <meronmks.8914@gmail.com>
2016-10-16 19:49:45 +0900

    暇だったんだ後悔なんて無い

--- app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
+++ app/src/main/java/com/meronmks/zimitta/Core/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(getString(R.string.Account), 0);
+        
         if(NetStatusUtil.isOnline(this) && !OauthUtils.hasAccessToken(this, preferences.getLong(getString(R.string.ActiveAccount), 0))){
             Intent intent = new Intent(this, TwitterOAuthActivity.class);
             intent.putExtra("Flag", false);
