a8837c1bf10f195b640df00d7bc05a0e0abe14cc
meronmks <meronmks.8914@gmail.com>
2016-10-19 00:17:26 +0900

    登録しているアカウント一覧を表示可能へ

--- app/src/main/java/com/meronmks/zimitta/Activity/AccountChangeActivity.java
+++ app/src/main/java/com/meronmks/zimitta/Activity/AccountChangeActivity.java
@@ -1,11 +1,29 @@
 package com.meronmks.zimitta.Activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
+import com.meronmks.zimitta.Adapter.AccountListAdapter;
 import com.meronmks.zimitta.Core.BaseActivity;
+import com.meronmks.zimitta.Core.MainActivity;
+import com.meronmks.zimitta.Datas.UserSetting;
+import com.meronmks.zimitta.OAuth.OauthUtils;
+import com.meronmks.zimitta.OAuth.TwitterOAuthActivity;
 import com.meronmks.zimitta.R;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.User;
 
 /**
  * Created by meron on 2016/10/16.
@@ -14,6 +32,8 @@ import com.meronmks.zimitta.R;
 public class AccountChangeActivity extends BaseActivity{
 
     private Toolbar toolbar;
+    private Twitter mTwitter;
+    private AccountListAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,5 +43,77 @@ public class AccountChangeActivity extends BaseActivity{
         toolbar.setTitle("アカウント選択");
         setSupportActionBar(toolbar);
 
+        adapter = new AccountListAdapter(this);
+
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.Account), 0);
+        long accountNum = preferences.getLong(getString(R.string.AccountNum), 0);
+
+        for (long i = 0; i < accountNum; i++) {
+            getUserItem(i);
+        }
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            SharedPreferences.Editor e = preferences.edit();
+            e.putLong(getString(R.string.ActiveAccount), i);
+            e.commit();
+            Intent intent = new Intent(AccountChangeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.acount_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.acount_Menu:
+                Intent intent = new Intent(this, TwitterOAuthActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode== KeyEvent.KEYCODE_BACK){
+            Intent intent = new Intent(AccountChangeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+    private void getUserItem(final long i) {
+        AsyncTask<Void, Void, User> task = new AsyncTask<Void, Void, User>() {
+
+            @Override
+            protected twitter4j.User doInBackground(Void... params) {
+                try {
+                    mTwitter = OauthUtils.getTwitterInstance(AccountChangeActivity.this, i);
+                    return mTwitter.verifyCredentials();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(twitter4j.User result) {
+                if (result != null) {
+                    adapter.add(result);
+                }
+            }
+        };
+        task.execute();
     }
 }
--- app/src/main/java/com/meronmks/zimitta/Adapter/AccountListAdapter.java
+++ app/src/main/java/com/meronmks/zimitta/Adapter/AccountListAdapter.java
@@ -1,8 +1,52 @@
 package com.meronmks.zimitta.Adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.meronmks.zimitta.R;
+
+import twitter4j.User;
+
 /**
  * Created by meron on 2016/10/18.
  */
 
-public class AccountListAdapter {
+public class AccountListAdapter extends ArrayAdapter<User> {
+    private LayoutInflater mInflater;
+
+    public AccountListAdapter(Context context) {
+        super(context, android.R.layout.simple_list_item_1);
+        mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.account_list_item, null);
+        }
+        User item = getItem(position);
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        name.setText(item.getName());
+        TextView screenName = (TextView) convertView.findViewById(R.id.atid);
+        screenName.setText("@" + item.getScreenName());
+        ImageView Icon = (ImageView)convertView.findViewById(R.id.icon);
+
+        try{
+            Glide.with(getContext()).load(item.getProfileImageURLHttps()).into(Icon);
+        }catch (Exception e){
+            e.getStackTrace();
+            Icon.setImageResource(R.mipmap.ic_sync_problem_white_24dp);
+            Log.e("GlideException", e.getMessage());
+        }
+        //super.getView(position, convertView, parent);
+        return convertView;
+    }
 }
