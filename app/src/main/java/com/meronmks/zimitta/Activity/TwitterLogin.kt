ebc7b78338e6eb1f292a117403ea1372757656e8
meronmks <meronmks.8914@gmail.com>
2017-06-25 00:27:11 +0900

    プロジェクトを再作成

---
+++ app/src/main/java/com/meronmks/zimitta/Activity/TwitterLogin.kt
@@ -0,0 +1,14 @@
+package com.meronmks.zimitta.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.meronmks.zimitta.R
+
+class TwitterLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_login)
+    }
+}
